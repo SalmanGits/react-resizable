4e445e571f51d7226706ec6baab09f14f3e833a6
SalmanGits <belalahmad10310@gmail.com>
2024-03-20 18:52:13 +0530

    feat:resizable components

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import ResizableComp from "./components/ResizableComp";
+import "./App.css";
+import "./style/Resizable.css";
+import "./style/Splitter.css";
+
+const App = () => {
+
+
+  return (
+    <div>
+
+      <ResizableComp />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/ResizableComp.jsx
@@ -0,0 +1,65 @@
+
+import Splitter from "./Splitter";
+import { useResizable } from "react-resizable-layout";
+import { cn } from "../utils/cn";
+
+const ResizableComp = () => {
+  const {
+    isDragging: isTerminalDragging,
+    position: terminalH,
+    splitterProps: terminalDragBarProps
+  } = useResizable({
+    axis: "y",
+    initial: 150,
+    min: 50,
+    reverse: true
+  });
+  const {
+    isDragging: isFileDragging,
+    position: fileW,
+    splitterProps: fileDragBarProps
+  } = useResizable({
+    axis: "x",
+    initial: 250,
+    min: 50
+  });
+
+
+  return (
+    <div
+      className={
+        "flex flex-column h-screen bg-dark font-mono color-white overflow-hidden"
+      }
+    >
+      <div className={"flex grow"}>
+        <div
+          className={cn("shrink-0 contents", isFileDragging && "dragging")}
+          style={{ width: fileW }}
+        >
+          Comp 1
+        </div>
+        <Splitter isDragging={isFileDragging} {...fileDragBarProps} />
+        <div className={"flex grow"}>
+          <div className={"grow bg-darker contents"}>Editor</div>
+
+        </div>
+      </div>
+      <Splitter
+        dir={"horizontal"}
+        isDragging={isTerminalDragging}
+        {...terminalDragBarProps}
+      />
+      <div
+        className={cn(
+          "shrink-0 bg-darker contents",
+          isTerminalDragging && "dragging"
+        )}
+        style={{ height: terminalH }}
+      >
+        Comp3
+      </div>
+    </div>
+  );
+};
+
+export default ResizableComp;
